ddbe09d10222e6eb9baf3087f90ea1464f8ae68f
sandra <nyamweasandra@gmail.com>
2023-04-04 01:59:22 +0300

    moving on well

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ const ul = document.querySelector('ul');
 icon.addEventListener('click', () => {
   ul.classList.toggle('active');
 });
+
+const container = document.querySelector('.image-container');
+
+fetch('https://api.unsplash.com/photos/random?count=10', {
+    headers: {
+        Authorization: 'Client-ID your-access-key-here'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    data.forEach(item => {
+        const img = document.createElement('img');
+        img.src = item.urls.regular;
+        img.alt = item.alt_description;
+        container.appendChild(img);
+        
+        // Add event listener to make images interactive
+        img.addEventListener('click', () => {
+            // Your code for what happens when image is clicked
+            img.classList.toggle('selected');
+        });
+    });
+})
+.catch(error => {
+    console.log(error);
+});
